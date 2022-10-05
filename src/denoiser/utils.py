60e4210a2c9065074b9aeb9cdc22add8acfc038e
Will <will@spokestack.io>
2022-10-05 13:23:08 -0400

    fp16

--- src/denoiser/utils.py
+++ src/denoiser/utils.py
@@ -10,8 +10,6 @@ def plot_image_batch(
     np_pred = pred.squeeze(1).cpu().detach().numpy()
 
     fig, ax = plt.subplots(len(clean), 3, figsize=(20, 5 * len(clean)))
-    fig.axes.get_xaxis().set_visible(False)
-    fig.axes.get_yaxis().set_visible(False)
     for i, (c, n, p) in enumerate(zip(np_clean, np_noisy, np_pred)):
         ax[i][0].imshow(c, origin="lower", aspect="auto")
         ax[i][1].imshow(n, origin="lower", aspect="auto")
