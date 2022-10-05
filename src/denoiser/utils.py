792c93e5ffa2caea2cc1bd61857316de56ed7b25
Will <will@spokestack.io>
2022-10-05 13:42:41 -0400

    fp16

--- src/denoiser/utils.py
+++ src/denoiser/utils.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import torch
 
+plt.axis("off")
+
 
 def plot_image_batch(
     clean: torch.Tensor, noisy: torch.Tensor, pred: torch.Tensor
 ) -> plt.Figure:
-    np_clean = clean.squeeze(1).cpu().detach().numpy()
-    np_noisy = noisy.squeeze(1).cpu().detach().numpy()
-    np_pred = pred.squeeze(1).cpu().detach().numpy()
+    np_clean = clean.squeeze(1).cpu().detach().numpy()[:5]
+    np_noisy = noisy.squeeze(1).cpu().detach().numpy()[:5]
+    np_pred = pred.squeeze(1).cpu().detach().numpy()[:5]
 
-    fig, ax = plt.subplots(len(clean), 3, figsize=(20, 5 * len(clean)))
+    fig, ax = plt.subplots(len(np_clean), 3, figsize=(20, 5 * len(np_clean)))
     for i, (c, n, p) in enumerate(zip(np_clean, np_noisy, np_pred)):
         ax[i][0].imshow(c, origin="lower", aspect="auto")
         ax[i][1].imshow(n, origin="lower", aspect="auto")
         ax[i][2].imshow(p, origin="lower", aspect="auto")
 
-    plt.axis("off")
     return fig
