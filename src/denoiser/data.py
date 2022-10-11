e7ac6eee9e1597dd4754d4c531e0ed05e381e6bf
Will <will@spokestack.io>
2022-10-11 11:40:06 -0400

    new waveunet architecture

--- src/denoiser/data.py
+++ src/denoiser/data.py
@@ -30,7 +30,7 @@ class LibriTTSDataModule(pl.LightningDataModule):
         self,
         data_dir: str,
         batch_size: int = 24,
-        max_length: int = 24000 * 3,
+        max_length: int = 16000,
         n_fft: int = 2048,
         win_length: int = 1024,
         hop_length: int = 256,
@@ -133,12 +133,13 @@ class LibriTTSDataModule(pl.LightningDataModule):
             audio_length = sample.size(0)
 
             if audio_length < self.max_length:
-                padding = self.max_length - audio_length
-                padded = F.pad(sample, (0, padding))
+                padded = F.pad(sample, (0, self.max_length))
             else:
                 padded = sample
 
-            random_idx = torch.randint(high=padded.size(0), size=(), low=0)
+            random_idx = torch.randint(
+                high=padded.size(0) - self.max_length, size=(), low=0
+            )
             padded = padded[random_idx : random_idx + self.max_length]
 
             noisy = torch.clone(padded)
