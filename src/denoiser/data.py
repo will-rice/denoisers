709847fc2812dc93aec7c21a198a1394a5f5d7c0
Will <will@spokestack.io>
2022-10-20 14:43:56 -0400

    new arc

--- src/denoiser/data.py
+++ src/denoiser/data.py
@@ -13,7 +13,7 @@ from torch.utils.data import DataLoader, Dataset
 
 from src.denoiser.transforms import RandomTransform
 
-MAX_LENGTH = 16384 * 10
+MAX_LENGTH = 24000 * 10
 
 
 class Sample(NamedTuple):
