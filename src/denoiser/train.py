6803fe7fdf4d908e558cd204cf00fa213b37e342
Will <will@spokestack.io>
2022-11-07 18:26:56 -0500

    fixed some stuff

--- src/denoiser/train.py
+++ src/denoiser/train.py
@@ -67,7 +67,6 @@ def main() -> None:
         precision=16 if torch.cuda.is_available() else 32,
         callbacks=[checkpoint_callback, swa_callback],
         track_grad_norm=True,
-        val_check_interval=100,
     )
     logger.watch(model)
 
