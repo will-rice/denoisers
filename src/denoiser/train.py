1b37d65e471a960a7eb49efb9c352257ec944353
Will <will@spokestack.io>
2022-11-13 19:31:45 -0500

    update

--- src/denoiser/train.py
+++ src/denoiser/train.py
@@ -20,7 +20,7 @@ def main() -> None:
     parser.add_argument(
         "--num_devices", default=1 if torch.cuda.is_available() else None
     )
-    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--batch_size", default=32, type=int)
     parser.add_argument("--dataset", default="libritts", type=str)
     parser.add_argument("--seed", default=1234, type=int)
     parser.add_argument("--debug", default=False, type=bool)
