5bb119a825b46f7fc58aa6daa671d58b97634ad3
Will <will@spokestack.io>
2023-09-25 14:10:47 -0400

    train vctk

--- denoisers/data/waveunet.py
+++ denoisers/data/waveunet.py
@@ -54,7 +54,7 @@ class AudioFromFileDataModule(pl.LightningDataModule):
             transforms.VolTransform(),
             transforms.FilterTransform(),
             transforms.ClipTransform(),
-            # transforms.BreakTransform(),
+            transforms.BreakTransform(),
             transforms.SpecTransform(),
             transforms.FreqNoiseMask(100, p=0.5),
             transforms.TimeNoiseMask(100, p=0.5),
