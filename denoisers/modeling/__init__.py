2c23c33e3bf768851f70a2f58b1fd2075b7781a9
Will <will@spokestack.io>
2023-10-11 15:31:21 -0400

    Fix top-level imports

--- denoisers/modeling/__init__.py
+++ denoisers/modeling/__init__.py
@@ -1,1 +1,5 @@
 """Models."""
+from denoisers.modeling.waveunet.config import WaveUNetConfig
+from denoisers.modeling.waveunet.model import WaveUNetModel
+
+__all__ = ["WaveUNetConfig", "WaveUNetModel"]
--- denoisers/modeling/waveunet/__init__.py
+++ denoisers/modeling/waveunet/__init__.py
@@ -1,1 +1,5 @@
 """1d UNet model."""
+from denoisers.modeling.waveunet.config import WaveUNetConfig
+from denoisers.modeling.waveunet.model import WaveUNetModel
+
+__all__ = ["WaveUNetConfig", "WaveUNetModel"]
