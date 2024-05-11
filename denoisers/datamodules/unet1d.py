596b9c793ab42ed3d9f225893624ed7c9cf5ed67
Will <will@spokestack.io>
2024-05-11 15:22:24 -0400

    Add support for sane sizes 16000, 32000, etc.

--- denoisers/datamodules/unet1d.py
+++ denoisers/datamodules/unet1d.py
@@ -72,8 +72,7 @@ class AudioFromFileDataModule(pl.LightningDataModule):
             if sr != self._sample_rate:
                 audio = torchaudio.functional.resample(audio, sr, self._sample_rate)
 
-            if audio.size(0) > 1:
-                audio = audio[0].unsqueeze(0)
+            audio = audio.mean(0).unsqueeze(0)
 
             audio_length = min(audio.size(-1), self._max_length)
 
