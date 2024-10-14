95bb001f84b48af50a31ba81368c45777f337129
Will <will@spokestack.io>
2024-10-13 22:11:55 -0400

    update

--- denoisers/modeling/unet1d/model.py
+++ denoisers/modeling/unet1d/model.py
@@ -73,7 +73,9 @@ class UNet1DLightningModule(LightningModule):
 
         snr = self.snr(outputs.audio, batch.audio)
         sdr = self.sdr(outputs.audio, batch.audio)
-        pesq = calculate_pesq(outputs.audio, batch.audio, self.config.sample_rate)
+        pesq = calculate_pesq(
+            outputs.audio.float(), batch.audio.float(), self.config.sample_rate
+        )
 
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_snr", snr)
