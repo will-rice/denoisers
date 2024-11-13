84bfdf5bdff117f6eb24f93fe45b06ecfb86dbb9
Will <will@spokestack.io>
2024-11-13 08:57:43 -0500

    Major refactor

--- denoisers/scripts/train.py
+++ denoisers/scripts/train.py
@@ -82,7 +82,7 @@ def main() -> None:
         precision="bf16-mixed",
         accumulate_grad_batches=2,
         callbacks=[checkpoint_callback, lr_monitor],
-        strategy="ddp_find_unused_parameters_true" if args.num_devices > 1 else "auto",
+        strategy="deepspeed" if args.num_devices > 1 else "auto",
     )
 
     trainer.fit(
